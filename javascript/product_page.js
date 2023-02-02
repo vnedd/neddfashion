872c677a00d2464841800eb69259bd9ffc7e8bc1
vnedd <thangvu2098@gmail.com>
2023-02-02 08:37:00 +0700

    secon

--- javascript/product_page.js
+++ javascript/product_page.js
@@ -126,3 +126,20 @@ productDetailsPanel.forEach((item, index) => {
     })
 
 })
+
+/// similar scrolling
+
+const similarWrapper = document.querySelector('.similar__wrapper');
+const similarPrevBtn = document.querySelector('.similar__direction-prev');
+const similarNextBtn = document.querySelector('.similar__direction-next');
+const similarProductItem = document.querySelector('.similar__item');
+
+similarPrevBtn.addEventListener('click', () => {
+    similarWrapper.scrollLeft -= similarProductItem.offsetWidth + 24;
+    console.log(similarWrapper.scrollLeft);
+})
+similarNextBtn.addEventListener('click', () => {
+    console.log(similarWrapper.scrollLeft);
+    similarWrapper.scrollLeft += similarProductItem.offsetWidth + 24;
+})
+
